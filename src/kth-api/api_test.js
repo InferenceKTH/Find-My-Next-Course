b0fdb10515dd0bb13e2b2d9d413296799089851e
daDevBoat <elias.richard.naess@gmail.com>
2025-03-27 16:38:12 +0100

    Added functionality for extracting active courses

--- src/kth-api/api_test.js
+++ src/kth-api/api_test.js
@@ -1,8 +1,12 @@
-async function KTH_API_fetch(course) {
+/* Takes in a string of the course code and returns the course code, name, location, department, prerequisits */
+async function KTH_API_course_fetch(course) {
     try {
         const response = await fetch("https://api.kth.se/api/kopps/v2/course/" + course + "/detailedinformation");
         const data = await response.json();
         const function_response = data["publicSyllabusVersions"][0]["courseSyllabus"]["eligibility"];
+
+        
+
         return function_response;
 
     } catch(err) {
@@ -11,12 +15,35 @@ async function KTH_API_fetch(course) {
 }
 
 
+/* Return an array of strings containing the course code of all active courses */
+async function KTH_API_all_active_courses() {
+    try {
+        let active_courses = [];
+        const response = await fetch("https://api.kth.se/api/kopps/v2/courses");
+        const data = await response.json();
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i]["state"] === "ESTABLISHED") {
+                active_courses.push(data[i]["code"]);
+            }
+        }
+        
+        return data
+        
+    } catch(err) {
+        console.error(err);
+
+    }
+}
+
 async function test() {
-    let resp = await KTH_API_fetch("IK1203");
+    let resp = await KTH_API_course_fetch("IK1203");
     console.log(resp);
 }
 
-test();
+//test();
+
+KTH_API_all_active_courses();
 
 
 
