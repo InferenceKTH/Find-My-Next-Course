95b1c3c2b044db64030db47230b1881611f3d780
Justus Kluge <justus.kluge@aikq.de>
2025-04-02 13:55:59 +0200

    Postponed in PR

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -71,27 +71,3 @@ export async function fetchCoursesSnapshot() {
     return snapshot.val();  // Return the object directly
 }
 
-export async function saveJSONCoursesToFirebase(model, data){
-    if(!data || !model){
-        console.log("no model or data")
-        return;
-    }
-    const entries = Object.entries(data);
-    entries.forEach(entry => {
-        const course = {code : entry[1].code , 
-            name: entry[1]?.name ?? "",
-            location: entry[1]?.location ?? "",
-            department: entry[1]?.department ?? "",
-            language: entry[1]?.language ?? "",
-            description: entry[1]?.description ?? "",
-            academicLevel: entry[1]?.academic_level ?? "",
-            period: entry[1]?.period ?? "",
-            credits: entry[1]?.credits ?? 0,
-            //lectureCount:entry[1].courseLectureCount,
-            //prerequisites:entry.coursePrerequisites
-            }
-            model.addCourse(course);
-            
-    });
-}
-
