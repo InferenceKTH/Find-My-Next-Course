5a2066b2aa2c07e273a1604a13e2268278a618cb
daDevBoat <elias.richard.naess@gmail.com>
2025-04-03 23:58:09 +0200

    More pruning again 1780 done

--- src/kth-api/eligibility_tester.js
+++ src/kth-api/eligibility_tester.js
@@ -9,53 +9,15 @@ let prereq_string = JSON.stringify(prereqs, null, 4)
 console.log(prereq_string);
 
 //function recursive_check
-let step = 0;
-function eligibility_check(courses_taken, prereq_object, previous_key) {
-    try {
-        let eligible;
-        if (Array.isArray(prereq_object)) {
-            let is_course_codes = true;
-            for (let i = 0; i < prereq_object.length; i++) {
-                let key = Object.keys(prereq_object[i])[0];
-                console.log(key);
-                if (key == "and" || key == "or") {
-                    is_course_codes = false;
-                    eligible = eligibility_check(courses_taken, prereq_object[i], key);
-                    if (eligible && key == "or") {console.log("hei"); return true;}
-                    if (!eligible && key == "and") {return false;}
-                    if (i == prereq_object.length - 1) {
-                        if (key == "or") {return false;}
-                        if (key == "and") {return true;}
-                    }
-                }
-            } 
-            
-            if (is_course_codes) {
-                console.log(previous_key + ": " + prereq_object);
-                let num_matches = 0;
-                
-                for (let i = 0; i < prereq_object.length; i++) {
-                    if (courses_taken.includes(prereq_object[i])) {num_matches++;}
-                }
-                
-                if ((previous_key == "or" && num_matches > 0) || (previous_key == "and" && num_matches == prereq_object.length)) {
-                    return true
-                }
-                return false
-            }
-        } else {
-            let key = Object.keys(prereq_object)[0];
-            console.log(key);
-            eligible = eligibility_check(courses_taken, prereq_object[key], key);
-        }
-        
-        //console.log(previous_key + ": " + eligible);
-        return eligible;
 
-    } catch (err) {
-        console.log(err);
+function eligibility_check(courses_taken, current_object, original_object, previous_key) {
+    if (!Array.isArray(current_object)) {
+        let keys = Object.keys(current_object);
+        console.log(keys)
+        for (let i = 0; i < keys.length; i++) {
+            eligibility_check(courses_taken, current_object[keys[i]], original_object, keys[i]);
+        }
     }
-    
 }
 
-console.log("Eligible: " + eligibility_check(test_courses_taken, prereqs, null));
+console.log("Eligible: " + eligibility_check(test_courses_taken, prereqs, prereqs, null));
