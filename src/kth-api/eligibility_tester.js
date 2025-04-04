54f424ee6e07fae7cf367f462b33801fc722a4fb
daDevBoat <elias.richard.naess@gmail.com>
2025-04-04 08:47:05 +0200

    Better working version

--- src/kth-api/eligibility_tester.js
+++ src/kth-api/eligibility_tester.js
@@ -3,23 +3,18 @@ import fs from "fs";
 let data = fs.readFileSync('all_courses_pruned.json');
 let data_object = JSON.parse(data);
 
-let test_courses_taken = ["SF1543", "DD1380", "DD1321"]
+let test_courses_taken = ["SF1543", "DD1380", "DD1321", "EP2720"]
 let prereqs = data_object["IK1203"]["prerequisites"];
 let prereq_string = JSON.stringify(prereqs, null, 4)
 console.log(prereq_string);
 
 //function recursive_check
 
-function eligibility_check(courses_taken, current_object, original_object, previous_key) {
-    if (!Array.isArray(current_object)) {
-        let keys = Object.keys(current_object);
-        //console.log(keys)
-        for (let i = 0; i < keys.length; i++) {
-            if (keys[i] == "or" || keys[i] == "and") {
-                eligibility_check(courses_taken, current_object[keys[i]], original_object, keys[i]);
-            } 
-        }
-
+function eligibility_check(courses_taken, current_object, previous_key) {
+    /* STEP 1: Sets courses taken to true, and not taken to false */
+    if (!Array.isArray(current_object)) {   // Is object
+        let key = Object.keys(current_object)[0];
+        eligibility_check(courses_taken, current_object[key], key);      
     } else {    // Is an array
         for (let i = 0; i < current_object.length; i++) {
             if (typeof current_object[i] == "string") {
@@ -28,19 +23,25 @@ function eligibility_check(courses_taken, current_object, original_object, previ
                 } else {
                     current_object[i] = false;
                 }
-            }
-            else if (!Array.isArray(current_object[i])) {
-                eligibility_check(courses_taken, current_object[i], original_object, previous_key);
-                //console.log(current_object[i])
+            } else {
+                eligibility_check(courses_taken, current_object[i], previous_key);
             }
         }
     }
-    if (previous_key == "or" && current_object.includes(true)) {
-        console.log("Hei")
+    
+    /* STEP 2: Check if an object is true or false based on content of the inner object */
+    if (typeof current_object == "object" && !Array.isArray(current_object)) {
+        let key = Object.keys(current_object)[0];
+        console.log(key);
+
+        
+
     }
+
+
+
 }
 
-console.log("Eligible: " + eligibility_check(test_courses_taken, prereqs, prereqs, null));
-
+console.log("Eligible: " + eligibility_check(test_courses_taken, prereqs, null));
 prereq_string = JSON.stringify(prereqs, null, 4)
 console.log(prereq_string);
