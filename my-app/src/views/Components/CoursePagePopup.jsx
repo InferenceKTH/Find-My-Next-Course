1959be81a9fe95cca49eb9a1a13f65da7604b06d
Kacper Lisik <lisik@kth.se>
2025-04-09 15:26:59 +0200

    some styling changes

--- my-app/src/views/Components/CoursePagePopup.jsx
+++ my-app/src/views/Components/CoursePagePopup.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 function CoursePagePopup({ isOpen, onClose, course, prerequisiteTree }) {
   if (!isOpen || !course) return null; // Don't render if not open or course not selected
-
   return (
     <div
       className="fixed backdrop-blur-sm inset-0 bg-transparent flex justify-end z-50"
       onClick={onClose}
     >
       <div
-        className="bg-indigo-400/70 backdrop-blur-sm h-full w-3/4 flex flex-col overflow-auto"
+        className="bg-indigo-300/70 backdrop-blur-lg h-full w-3/4 flex flex-col overflow-auto"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex-1">
           <div className="px-10 py-10 md:px-20 md:py-16 text-slate-900 space-y-12 font-sans">
             {/* Course Title Section */}
             <div>
-              <h2 className="text-5xl font-extrabold text-[#2e2e4f]">
-                {course.code} - {course.name}
-                <span className="ml-4 text-lg text-violet-900">({course.credits} Credits)</span> {/* Display Credits */}
+              <h2 className="text-5xl font-extrabold text-[#2e2e4f] ">
+                <span className="text-violet-700">{course.code}</span> - {course.name}
+                <span className="ml-4 text-lg text-violet-700">({course.credits} Credits)</span>
               </h2>
+              <div className="my-6 h-1.5 w-full bg-violet-500"></div>
             </div>
             <div>
               <button
@@ -37,11 +37,13 @@ function CoursePagePopup({ isOpen, onClose, course, prerequisiteTree }) {
 
             {/* Description Section */}
             <div>
-              <h3 className="text-2xl font-bold text-[#3d3d68] mb-4">Course Description</h3>
+              <h3 className="text-2xl font-bold text-[#3d3d68] mb-0">Course Description</h3>
+              <div className="mb-3 h-0.5 w-full bg-violet-500"></div>
               <div
                 className="text-lg leading-8 text-[#2c2c2c] tracking-wide prose prose-slate max-w-full"
                 dangerouslySetInnerHTML={{ __html: course.description }}
               />
+               
             </div>
 
             {/* Prerequisite Graph Tree Section */}
