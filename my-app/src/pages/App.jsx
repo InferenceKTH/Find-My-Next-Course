267c180542e1f6193aaa9ef59352fc85ad5f095b
Sami Al Saati <sami.alsaati@hotmail.com>
2025-04-10 16:17:23 +0200

    fixed scrolling

--- my-app/src/pages/App.jsx
+++ my-app/src/pages/App.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { SidebarPresenter } from "../presenters/SidebarPresenter.jsx";
-import { SearchbarPresenter } from "../presenters/SearchbarPresenter.jsx";
+import { SidebarPresenter } from '../presenters/SidebarPresenter.jsx';
+import { SearchbarPresenter } from '../presenters/SearchbarPresenter.jsx';
 import { ListViewPresenter } from '../presenters/ListViewPresenter.jsx';
-import {model} from '/src/model.js';
+import { model } from '/src/model.js';
 
 function App() {
 	return (
-		<div className="flex h-screen w-screen">
-			<div className="flex-auto w-40% h-full bg-gradient-to-t from-[#4f3646] to-[#6747c0]">
+		<div className="flex h-screen w-screen overflow-hidden">
+			<div className="w-1/4 h-full bg-gradient-to-t from-[#4f3646] to-[#6747c0]">
 				<SidebarPresenter model={model} />
-
 			</div>
 			<div className="w-3/4 h-full flex flex-col">
-				<div className="bg-gradient-to-t from-[#6246a8] to-[#6747c0] text-white" style={{color: "red"}}>
-				{/* bg-gradient-to-t from-[#6246a8] to-[#6747c0] */}
+				<div className="bg-gradient-to-t from-[#6246a8] to-[#6747c0] text-white">
 					<SearchbarPresenter model={model} />
 				</div>
-				<div className="flex-auto border overflow-auto bg-[#121212]">
+				<div className="flex-auto border bg-[#121212] relative">
 					<ListViewPresenter model={model} />
 				</div>
 			</div>
