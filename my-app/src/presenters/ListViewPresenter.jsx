cda406540eb0809d118904bb9b94391a252c56d2
daDevBoat <elias.richard.naess@gmail.com>
2025-04-11 13:26:07 +0200

    Test

--- my-app/src/presenters/ListViewPresenter.jsx
+++ my-app/src/presenters/ListViewPresenter.jsx
@@ -4,15 +4,9 @@ import { useState } from 'react';
 import ListView from "../views/ListView.jsx";
 import CoursePagePopup from '../views/Components/CoursePagePopup.jsx';
 import PrerequisitePresenter from './PrerequisitePresenter.jsx';
-import {ReviewPresenter} from "../presenters/ReviewPresenter.jsx";
+import {ReviewPresenter} from "../presenters/ReviewPresenter.jsx"
 
 const ListViewPresenter = observer(({ model }) => {
-
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-    const [selectedCourse, setSelectedCourse] = useState(null);
-    const preP = <PrerequisitePresenter model={model} selectedCourse={selectedCourse}/>
-    const popup = <CoursePagePopup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)} course={selectedCourse} prerequisiteTree={preP}/>
-
     const addFavourite = (course) => {
         model.addFavourite(course);
     }
