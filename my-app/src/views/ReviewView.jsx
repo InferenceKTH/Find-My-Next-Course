cda8037d674255d8f8b1ecb1ab3331f05d56be5f
emyemy89 <emiparaschiv345@gmail.com>
2025-04-11 11:08:33 +0200

    added limit and changed design for multiple rows

--- my-app/src/views/ReviewView.jsx
+++ my-app/src/views/ReviewView.jsx
@@ -13,13 +13,22 @@ export function ReviewView({
 		<div>
 			{/* Reviews Section */}
 			<div className="mt-4">
-				<input
-					type="text"
-					placeholder="Your review"
-					className="border rounded p-2 w-full"
-					value={newReview}
-					onChange={(e) => setNewReview(e.target.value)}
+				<div className="relative">
+				<textarea
+				rows={6}
+				placeholder="Your review (max 2500 characters)"
+				className="resize-none border rounded-lg p-3 w-full pr-14 focus:outline-none focus:ring-2 focus:ring-blue-400 shadow-sm transition duration-200"
+				value={newReview}
+				onChange={(e) => {
+					const trimmed = e.target.value.slice(0, 2500);
+					setNewReview(trimmed);
+				}}
 				/>
+					<span className="absolute bottom-1 right-3 text-sm text-gray-500">
+						{newReview.length}/2500
+					</span>
+				</div>
+
 				<button
 					className="mt-2 bg-violet-600 text-white py-1 px-4 rounded"
 					onClick={handleReviewSubmit}
