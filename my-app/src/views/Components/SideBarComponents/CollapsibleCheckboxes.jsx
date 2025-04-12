5105d0c0454ffe61f4a69103e59c2913414346f9
kexana <deotsts@gmail.com>
2025-04-12 19:06:06 +0200

    beautius department filter

--- my-app/src/views/Components/SideBarComponents/CollapsibleCheckboxes.jsx
+++ my-app/src/views/Components/SideBarComponents/CollapsibleCheckboxes.jsx
@@ -5,6 +5,7 @@ const CollapsibleCheckboxes = (props) => {
   const [expanded, setExpanded] = useState({});
   const [filterEnabled, setFilterEnabled] = useState(true);
   const [checkedSubItems, setCheckedSubItems] = useState({});
+  const [stupidLines, setStupidLines] = useState(0);
 
 
 
@@ -14,8 +15,8 @@ const CollapsibleCheckboxes = (props) => {
       [id]: !prev[id],
     }));
     subItems.map((_, index) => {
-      setSubCheckbox(id,index)
-     });
+      setSubCheckbox(id, index)
+    });
   };
 
   const setSubCheckbox = (mainId, index) => {
@@ -47,6 +48,11 @@ const CollapsibleCheckboxes = (props) => {
     },
     {
       id: 3,
+      label: "Category 2",
+      subItems: ["Sub-item 2.1", "Sub-item 2.2"],
+    },
+    {
+      id: 4,
       label: "Category 3",
       subItems: [
         "Sub-item 3.1",
@@ -78,11 +84,10 @@ const CollapsibleCheckboxes = (props) => {
         />
       </div>
       <div
-        className={`${
-          filterEnabled
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-50 pointer-events-none"
-        } transition-all`}
+        className={`${filterEnabled
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-50 pointer-events-none"
+          } transition-all`}
       >
         <div className="rounded-lg shadow-2xs w-full text-white bg-[#aba8e0] border border-gray-200 p-4">
           {rows.map((row) => (
@@ -100,43 +105,77 @@ const CollapsibleCheckboxes = (props) => {
                 </label>
               </div>
 
+              <svg
+                width="40"
+                height={`${expanded[row.id] ? (row.subItems.length) * 24 + 35 : 30}`}
+                viewBox={`0 0 40 ${expanded[row.id] ? (row.subItems.length) * 24 + 35 : 30}`}
+                preserveAspectRatio="none"
+                className="absolute left-[-25px] top-[-5px]"
+              >
+                <path
+                  d={`M20 0 V${(row.subItems.length) * 24 + 35}`}
+                  stroke="white"
+                  strokeWidth="5"
+                  fill="none"
+                  className=""
+                />
+                {row.id === 1 && (
+                  <path
+                  d={`M20 2 H33`}
+                  stroke="white"
+                  strokeWidth="5"
+                  fill="none"
+                  className=""
+                />
+                )}
+                {row.id === rows.length && (
+                  <path
+                  d={`M20 ${(expanded[row.id]? row.subItems.length:1) * 30 -3} H33`}
+                  stroke="white"
+                  strokeWidth="5"
+                  fill="none"
+                  className=""
+                />
+                )}
+                
+              </svg>
+
               {expanded[row.id] && (
-                <div className="mt-2 relative">
+                <div className="mt-2 relative ">
+                  <svg
+                    width="40"
+                    height={`${(row.subItems.length) * 24}`}
+                    viewBox={`0 0 40 ${(row.subItems.length) * 24}`}
+                    preserveAspectRatio="none"
+                    className="absolute left-[-14px] top-[-10px] transition-all duration-300 ease-in-out stroke-animate"
+                  >
+                    <path
+                      d={`M20 0 V${(row.subItems.length) * 32}`}
+                      stroke="white"
+                      strokeWidth="5"
+                      fill="none"
+                      className="transition-all duration-300 ease-in-out stroke-animate"
+                    />
+                  </svg>
                   {row.subItems.map((subItem, index) => {
                     const checkboxId = `sub-checkbox-${row.id}-${index}`;
                     const key = `${row.id}-${index}`;
-                    const y = index * 32;
-
-                    <svg
-                    width="40"
-                    height={row.subItems.length * 50}
-                    viewBox={`0 0 200 200`}
-                            className="absolute left-[-40px] top-1 transition-all duration-300 ease-in-out stroke-animate"
-                          >
-                            <path
-                              d={`M0 0 V200 `}
-                              stroke="white"
-                              strokeWidth="1.5"
-                              fill="none"
-                              className="transition-all duration-300 ease-in-out stroke-animate"
-                            />
-                          </svg>
 
                     return (
                       <div key={checkboxId} className="relative pl-6 flex items-center">
                         {/* SVG line only if the checkbox is checked */}
                         {checkedSubItems[key] && (
                           <svg
-                            width="100"
-                            height="50"
-                            viewBox="10 0 40 50"
+                            width="40"
+                            height="40"
+                            viewBox="0 0 40 50"
                             preserveAspectRatio="none"
-                            className="absolute left-[-40px] top-1 transition-all duration-300 ease-in-out stroke-animate"
+                            className="absolute left-[-14px] top-1 transition-all duration-300 ease-in-out stroke-animate"
                           >
                             <path
-                              d="M20 5 H30"
+                              d="M20 10 H33"
                               stroke="white"
-                              strokeWidth="2"
+                              strokeWidth="5"
                               fill="none"
                               className="transition-all duration-300 ease-in-out stroke-animate"
                             />
